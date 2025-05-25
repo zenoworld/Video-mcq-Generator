a1589255af384c4d9ea285908fcddc5be4232640
zenoworld <subhromajumder52@gmail.com>
2025-05-25 23:55:20 +0530

    first commit

--- video_mcq_generator_back/src/services/mcq.service.ts
+++
@@ -1,6 +0,0 @@
-import axios from "axios"
-
-export const generateMCQs = async (segments: string[]) => {
-  const res = await axios.post("http://localhost:8000/generate-questions", { segments })
-  return res.data.mcqs
-}
--- video_mcq_generator_back/src/services/transcription.service.ts
+++
@@ -1,6 +0,0 @@
-import axios from "axios"
-
-export const transcribeVideo = async (videoPath: string) => {
-  const res = await axios.post("http://localhost:8000/transcribe", { videoPath })
-  return res.data
-}
